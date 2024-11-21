2c12be8b5bbb5008064de7acff29287fd08dcf90
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-11-21 09:50:22 -0700

    Create test module calling out to SMBC binary

---
+++ src/main.py
@@ -0,0 +1,76 @@
+import pathlib
+import subprocess
+import tempfile
+import typing
+
+import click
+import staliro
+import staliro.models as models
+import staliro.optimizers as optimizers
+import staliro.specifications.rtamt as rtamt
+
+Command: typing.TypeAlias = typing.Literal["0,0", "0,1", "1,0", "1,1"]
+
+
+def _parse_position(line: str) -> list[float]:
+    return list(map(float, line.split(",")))
+
+
+def _convert_joystick(angle: float) -> Command:
+    if angle < 90:
+        return "0,0"
+
+    if 90 <= angle < 180:
+        return "0,1"
+
+    if 180 <= angle < 270:
+        return "1,0"
+
+    return "1,1"
+
+
+@models.blackbox(step_size=1.0)
+def smbc(sample: models.Blackbox.Inputs) -> models.Trace[list[float]]:
+    with tempfile.TemporaryFile("w") as input_file:
+        input_file.writelines(
+            _convert_joystick(state["joystick"]) for state in sample.times.values()
+        )
+
+        cwd = pathlib.Path("utils")
+        proc = subprocess.run(
+            args="./smbc 0 trace",
+            stdin=input_file,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+            shell=True,
+            cwd=cwd.resolve(),
+        )
+
+        if proc.returncode != 0:
+            raise RuntimeError()
+
+        positions = [
+            _parse_position(line) for line in proc.stdout.splitlines() if "," in line
+        ]
+
+    return models.Trace(times=list(sample.times), states=positions)
+
+
+@click.command()
+@click.option("-i", "--iterations", type=int, default=100)
+@click.option("-f", "--frames", type=int, default=100)
+def main(iterations: int, frames: int):
+    req = rtamt.parse_dense("always (x < 100)", {"x": 0, "y": 1})
+    opts = staliro.TestOptions(
+        tspan=(0, frames),
+        iterations=iterations,
+        signals={"joystick": staliro.SignalInput(control_points=[(0, 360)]*frames)}
+    )
+    opt = optimizers.UniformRandom()
+    results = staliro.test(smbc, req, opt, opts)
+
+    print(results)
+
+
+if __name__ == "__main__":
+    main()
