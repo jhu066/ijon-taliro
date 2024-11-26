cf1dcbd816ec0beaf7440f3bfe48ede645ce8577
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-11-26 08:07:13 -0700

    Refactor visualize script to use output pickle file

--- src/visualize.py
+++ src/visualize.py
@@ -1,52 +1,50 @@
-import os
-import glob
-import sys
-from xml.sax.saxutils import escape
+import itertools
+import pathlib
+import pickle
 
-# Read the SVG file
-with open("mario-1-1.svg", "r") as svg_file:
-    svg = svg_file.read()
+import click
+import staliro
 
-workdir = sys.argv[1]
-inputs = glob.glob(os.path.join(workdir, "*"))
+import main
 
-# with open("./uniq.log", "r") as fp:
-#     lines = fp.readlines()
-# inputs = [os.path.join("./outputs", line.strip()) for line in lines]
 
-print(len(inputs))
-paths = {}
-
-# load all the paths' trace of coordinates
-for input in inputs:
-    with open(input, "r") as fp:
-        lines = fp.readlines()
-    lines = [
-        tuple(map(float, line.strip().split(",")))
-        for line in lines
-        if line.strip()
-        # and "," in line
-    ]
-    data = [(x, y) for x, y in lines if x!=0 and y!= 0]
-    paths[input] = data
-
-paths_list = []
-# generate svg path element
-for input in inputs:
+def _create_svg_path(name: str, trace: staliro.Trace[main.Line]):
     color = "#f8bf00"
     alpha = "0.2"
-    data = paths[input]
+    data = [(state["x"], state["y"]) for state in trace.states]
     path_data = " ".join(f"L{x + 8} {y + 16}" for x, y in data)
     path_element = (
         f'<path fill="none" stroke="{color}" stroke-opacity="{alpha}" '
         f'stroke-width="3" d="M40 176{path_data}">'
-        f'<title>{escape(input)}</title></path>'
+        f'<title>{name}</title></path>'
     )
-    paths_list.append(path_element)
+
+    return path_element
 
 
-print(len(paths_list))
-# replace placeholder in SVG and write to file
-content = svg.replace("{{PATHS}}", "\n".join(paths_list))
-with open("test2.svg", "w") as output_file:
-    output_file.write(content)
+@click.command("visualize")
+@click.argument("FILE", type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path))
+def visualize(file: pathlib.Path):
+    with file.open("rb") as pickle_file:
+        runs = pickle.load(pickle_file)
+
+    paths_list = [
+        _create_svg_path(f"{n}.trace", e.extra.trace)
+        for n, e in enumerate(itertools.chain.from_iterable(run.evaluations for run in runs))
+    ]
+
+    svg_path = pathlib.Path("data/mario-1-1.svg")
+
+    # Read the SVG file
+    with svg_path.open("r") as svg_file:
+        svg = svg_file.read()
+
+    # replace placeholder in SVG and write to file
+    content = svg.replace("{{PATHS}}", "\n".join(paths_list))
+
+    with open("mario-1-1.svg", "w") as output_file:
+        output_file.write(content)
+
+
+if __name__ == "__main__":
+    visualize()
